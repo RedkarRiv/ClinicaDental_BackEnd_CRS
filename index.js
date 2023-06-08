2f1cfc9c3055fb4f1a69c2d4862e974246841162
Carlos <carresan.val@gmail.com>
2023-06-08 12:12:45 +0200

    INIT: Comit inicial. Estructura creada y tabla de Role y User creada

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const db = require('./db');
+const router = require('./router');
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use(router);
+
+app.get('/health', (req, res) => {
+    return res.send('healthy');
+});
+
+
+db .then(() =>
+    {
+        app.listen(PORT, () => {
+            console.log('Server is running on port: ' + PORT);
+        })
+    }
+).catch((error) => {
+    console.error('Error starting server', error.message);
+})
---
+++ migrations/20230608100141-create-user.js
@@ -0,0 +1,56 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      surname: {
+        type: Sequelize.STRING
+      },
+      dni: {
+        type: Sequelize.STRING
+      },
+      adress: {
+        type: Sequelize.STRING
+      },
+      birthdate: {
+        type: Sequelize.DATE
+      },
+      phone: {
+        type: Sequelize.INTEGER
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      role_id: {
+        type: Sequelize.INTEGER,
+        references:{
+          model: "Roles",
+          key: "id"
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
