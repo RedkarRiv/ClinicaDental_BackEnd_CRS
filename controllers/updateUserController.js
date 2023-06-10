4ea67389811ad0613d8e067add652148a1c6514b
Carlos <carresan.val@gmail.com>
2023-06-10 18:33:19 +0200

    UPDATE: GetOneUser creado. UpdateUser creado y pte de correccion de errores

---
+++ controllers/updateUserController.js
@@ -0,0 +1,77 @@
+const { User } = require("../models");
+const bcrypt = require("bcrypt");
+const updateUserController = {};
+
+updateUserController.updateUser = async (req, res) => {
+
+try {
+    const userId = req.userId;
+const user = await User.findByPk(userId);
+
+if (!user) {
+    return res.json(
+        {
+            success: true,
+            message: "El usuario no existe"
+        }
+    )
+}
+
+        const name = req.body.name
+        const surname = req.body.surname
+        const dni = req.body.dni
+        const address = req.body.address
+        const birth_date = req.body.birth_date
+        const phone = req.body.phone
+        const email = req.body.email
+        const password = req.body.password
+
+        // if (password.length < 4) {
+        //     return res.json(
+        //         { 
+        //             success: false,
+        //             message:"La contraseña debe tener al menos 4 carácteres. Inténtelo de nuevo"
+        //         }
+        //         )
+        // }
+
+        // const newPassword = bcrypt.hashSync(password, 10);
+
+        const result = await User.update (
+            {
+            name : name,
+            surname : surname,
+            dni : dni,
+            address : address,
+            birth_date : birth_date,
+            phone : phone,
+            email : email,
+            password: password
+            },
+            {
+                where: {
+                    id:userId
+                }
+            })
+
+            return res.json(
+                {
+                    success: true,
+                    message: "Datos del usuario actualizados",
+                    data: result
+                }
+            )
+
+
+} catch (error) {
+    return res.status(500).json(
+        {
+            success: false,
+            message: "No se ha podido actualizar los datos del usuario",
+            error: error.message
+        }
+    )
+}
+}
+
+module.exports = updateUserController
--- views/userRoutes.js
+++ views/userRoutes.js
@@ -1,9 +1,13 @@
 const router = require("express").Router();
 const auth = require("../middlewares/tokenVerify");
 const deleteUserController = require("../controllers/deleteUserController");
+const updateUserController = require("../controllers/updateUserController");
 const isAdmin = require("../middlewares/isAdmin");
+const getOneUserController = require("../controllers/getOneUserController");
 
 router.delete("/:id", auth, isAdmin, deleteUserController.deleteUser);
+router.put("/update", auth, updateUserController.updateUser);
+router.get("/", auth, getOneUserController.getOneUser);
 
 
 module.exports = router;
