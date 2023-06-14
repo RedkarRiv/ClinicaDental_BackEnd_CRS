d7863b6850818e66659ea3710a8a86693d0b33ce
Carlos <carresan.val@gmail.com>
2023-06-14 23:44:20 +0200

    UPDATE: Primer try para endPoint de creación de Empleados v2

--- views/authRoutes.js
+++ views/authRoutes.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
+const isAdmin = require("../middlewares/isAdmin");
+
 const authController = require('../controllers/authControllers/authController')
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
-router.post('/newEmployee', authController.createEmployee);
+router.post('/newEmployee', isAdmin, authController.createEmployee);
 
 module.exports = router;
