b953c4e6dc07e041871d756a96af425504add274
Carlos <carresan.val@gmail.com>
2023-06-14 23:38:41 +0200

    UPDATE: Primer try para endPoint de creación de Empleados

--- controllers/authControllers/authController.js
+++ controllers/authControllers/authController.js
@@ -1,4 +1,4 @@
-const { User } = require("../../models");
+const { User, Employee } = require("../../models");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
@@ -98,6 +98,61 @@ authController.login = async (req, res) => {
       message: "El usuario no ha podido logearse",
       error: error.menssage,
     });
+  }
+};
+
+authController.createEmployee = async (req, res) => {
+  try {
+  const name = req.body.name;
+  const surname = req.body.surname
+  const profId = req.body.profesional_registration_id
+  const spec = req.body.speciality_id
+  const email = req.body.email;
+  const password = req.body.password;
+  const checkEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{4,}$/;
+
+  if (!checkEmail.test(req.body.email)) {
+    return res.status(400).json({
+      success: false,
+      message: "El correo no es valido",
+    });
+  }
+
+  if (!regex.test(req.body.password)) {
+    return res.json(
+      {
+        success: true,
+        message: "La contraseña debe tener una mayuscula, una minuscula y un número. Su longitud nunca puede ser inferior a 4."
+      }
+    );
+  }
+
+    const newPassword = bcrypt.hashSync(req.body.password, 8);
+    const newUser = await User.create({
+      name: req.body.name,
+      surname: req.body.surname,
+      email: req.body.email,
+      password: newPassword,
+      role_id: 2,
+    });
+    const newEmployee = await Employee.create({
+      role_id: 2,
+      specilaty_id: spec,
+      profesional_registrarion_id: profId,
+      password: newPassword,
+      active_status: true,
+      user_id: newUser.id
+    });
+    return res.send(newUser);
+  } catch (error) {
+    return res.json(
+      {
+        success: false,
+        message: "No ha sido posible crear la cuenta",
+        error: error.message
+      }
+      );
   }
 };
 
--- views/authRoutes.js
+++ views/authRoutes.js
@@ -3,5 +3,6 @@ const authController = require('../controllers/authControllers/authController')
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+router.post('/newEmployee', authController.createEmployee);
 
 module.exports = router;
