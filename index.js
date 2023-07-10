d7dbd1ef99e3990e39905b3016a8fcf054b88997
Carlos <carresan.val@gmail.com>
2023-07-10 19:51:57 +0200

    FIX: .env de PORT eliminada y seteada para null en index

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const router = require('./router');
 const app = express();
 require ("dotenv").config()
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 
 app.use(cors());
