b9469f9bc90c804ef3e2db83c5f805f3b41b790f
Carlos <carresan.val@gmail.com>
2023-07-15 19:54:01 +0200

    FIX: CAMbiado HOST de RAILWAY DDBB en config.js

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ module.exports = {
     "username": process.env.USER,
     "password": process.env.PASSWORD,
     "database": process.env.DDBBNAME,
-    "host": "containers-us-west-167.railway.app",
+    "host": "mysql://root:EtuQUDHpQgo8kKkZEiBG@containers-us-west-167.railway.app:7157/railway",
     "dialect": "mysql",
     "port": process.env.MYSQL_PORT
   },
@@ -21,7 +21,7 @@ module.exports = {
     "username": process.env.USER,
     "password": process.env.PASSWORD,
     "database": process.env.DDBBNAME,
-    "host": "containers-us-west-167.railway.app",
+    "host": "mysql://root:EtuQUDHpQgo8kKkZEiBG@containers-us-west-167.railway.app:7157/railway",
     "dialect": "mysql",
     "port": process.env.MYSQL_PORT
     
